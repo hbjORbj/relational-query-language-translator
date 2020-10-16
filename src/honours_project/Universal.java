382c0801c0b88c8a757433e70f83c2a41e02e366
hbjORbj <sldisek783@gmail.com>
2020-10-16 18:14:08 +0100

    Universal Class Update

--- src/honours_project/Universal.java
+++ src/honours_project/Universal.java
@@ -1,13 +1,15 @@
 package honours_project;
 
 import java.util.List;
+import java.util.Set;
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Universal extends Expression {
 	private Expression operand;
-	private List<String> terms;
+	private List<Term> terms;
 	
-	public Universal (List<String> terms, Expression operand) {
+	public Universal (List<Term> terms, Expression operand) {
 		super(Expression.Type.UNIVERSAL);
 		this.operand = operand;
 		this.terms = new ArrayList<>();
@@ -16,9 +18,23 @@ public class Universal extends Expression {
 	
 	@Override
 	public String toString() {
+		List<String> list = new ArrayList<>();
+		for (Term t : terms) {
+			list.add(t.toString());
+		}
 		return String.format("%s%s( %s )",
 				this.getType().getConnective(),
-				String.join(",", terms),
+				String.join(",", list),
 				operand.toString());
+	}
+	
+	public Set<Term> free() {
+		Set<Term> free = new HashSet<>();
+		for (Term t : terms) {
+			if (t.isVariable()) {
+				free.add(t);
+			}
+		}
+		return free;
 	}
 }
