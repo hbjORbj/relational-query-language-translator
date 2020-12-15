fe38eed8e43f710ab118291292fa6fc53f42e8c8
hbjORbj <sldisek783@gmail.com>
2020-12-15 14:52:26 +0000

    RA to RC Basic Translation

--- src/main/java/s1705270/TranslatorRC.java
+++ src/main/java/s1705270/TranslatorRC.java
@@ -22,7 +22,7 @@ public class TranslatorRC {
 	
 	public TranslatorRC(Schema sch) {
 		this.schema = sch;
-		this.env  = new HashMap<>();
+		this.env  = new HashMap<>(); // Variable -> Attribute
 	}
 	
 	private Expression Adom(String name) { // needs the schema as well
@@ -49,6 +49,55 @@ public class TranslatorRC {
 		return new Intersection(e1, e2);
 	}
 	
+	private Expression disjunctionToRA(Disjunction disj) {
+		Formula f1 = disj.getLeftOperand();
+		Formula f2 = disj.getRightOperand();
+		Set<Term> free1 = f1.free();
+		Set<Term> free2 = f2.free();
+		Expression e1 = translateToRA(f1);
+		Expression e2 = translateToRA(f2);
+		for (Term term : free2) {
+			if (!free1.contains(term)) {
+				e1 = new Product(e1, Adom(env.get(term.toString())));
+			}
+		}
+		for (Term term : free1) {
+			if (!free2.contains(term)) {
+				e2 = new Product(e2, Adom(env.get(term.toString())));
+			}
+		}
+		return new Union(e1, e2);
+	}
+	
+	private Expression predicateToRA(Predicate pred) {
+		String name = pred.getName();
+		List<Term> terms = pred.getTerms();
+		
+		Expression relation = new BaseExpression(name);
+		Map<String,String> replacements = new HashMap<>();
+		for (Term term : terms) {
+			replacements.put(env.get(term.toString()), term.toString());
+		}
+		
+		return new Renaming(replacements, relation); 
+	}
+	
+	private Expression existentialToRA(Existential ext) {
+		Formula f = ext.getOperand();
+		Expression e = translateToRA(f);
+
+		List<Term> terms = ext.getTerms();
+		Set<Term> free = ext.free();
+		List<String> attributes;
+		for (Term term: free) {
+			if (!terms.contains(term)) {
+				attributes.add(term.toString());
+			}
+		}
+		
+		return new Projection(attributes, e);
+	}
+	
 	private Expression equalityToRA(Equality eq) {
 		uk.ac.ed.pguaglia.real.lang.Term t1 = new uk.ac.ed.pguaglia.real.lang.Term(env.get(eq.getLeftTerm().toString()), false);
 		uk.ac.ed.pguaglia.real.lang.Term t2 = new uk.ac.ed.pguaglia.real.lang.Term(env.get(eq.getRightTerm().toString()), false);
@@ -60,72 +109,11 @@ public class TranslatorRC {
 	    if (f instanceof Conjunction) {
 			return conjunctionToRA((Conjunction) f);
 	    } else if (f instanceof Disjunction) {
-	    		Disjunction disj = (Disjunction) f;
-	    		Formula f1 = disj.getLeftOperand();
-	    		Formula f2 = disj.getRightOperand();
-	    		Set<Term> free1 = f1.free();
-	    		Set<Term> free2 = f2.free();
-	    		Stack<Expression> temp1 = new Stack<>();
-	    		Stack<Expression> temp2 = new Stack<>();
-	    		for (Term term : free2) {
-	    			if (!free1.contains(term)) {
-	    				if (temp1.size() > 0) {
-	    					Expression product = Product(temp1.pop(), Adom(""));
-	    					temp1.push(Adom(product)); // Adom() hasn't yet been written
-	    				} else {
-	    					temp1.push(Adom(term));
-	    				}
-	    			}
-	    		}
-	    		for (Term term : free1) {
-	    			if (!free2.contains(term)) {
-	    				if (temp2.size() > 0) {
-	    					Expression product = Product(temp2.pop(), term);
-	    					temp2.push(Adom(product));
-	    				} else {
-	    					temp2.push(Adom(term));
-	    				}
-	    			}
-	    		}
-	    		
-	    		Expression e1 = Translator.translateToRA(f1);
-	    		Expression e2 = Translator.translateToRA(f2);
-	    		Expression adomProduct1;
-	    		Expression adomProduct2;
-	    		
-	    		e1 = Product(e1, temp1.pop());
-	    		e2 = Product(e2, temp2.pop());
-	    		Expression e = Union(e1, e2);
-	    		return e;
+			return disjunctionToRA((Disjunction) f);
 	    } else if (f instanceof Existential) {
-	    		Existential exist = (Existential) f;
-	    		Formula f1 = exist.getOperand();
-	    		Expression e1 = translateToRA(f1);
-
-	    		List<Term> terms = exist.getTerms();
-	    		Set<Term> free = exist.free();
-	    		List<String> attributes;
-	    		for (Term term: free) {
-	    			if (!terms.contains(term)) {
-	    				attributes.add(term.toString());
-	    			}
-	    		}
-	    		
-	    		Expression e = Projection(attributes, e1);
-	  	    return e;
+	    		return existentialToRA((Existential) f);
 	    } else if (f instanceof Predicate) {
-	    		Predicate predic = (Predicate) f;
-	    		String name = predic.getName();
-	    		List<Term> terms = predic.getTerms();
-	    		
-	    		Expression operand = new BaseExpression(name);
-	    		Map<String,String> replacements = new HashMap<>();
-	    		for (Term term : terms) {
-	    			replacements.put("?", term.toString()); // what should go into "?"
-	    		}
-	    		
-	    		Expression e = Renaming(replacements, operand); 
-	  	    return e;
+	    		return predicateToRA((Predicate) f);
 	    } else if (f instanceof Equality) {
 	    		return equalityToRA((Equality) f);
 	    } else { // we should never reach this point
@@ -133,8 +121,51 @@ public class TranslatorRC {
 	    }
 	  }
 
-	  public static Formula translateToRC( Expression e ) {
-	    // similar stuff appropriate for RA->RC
-	    return null;
-	  }
+	private Formula projectionToRC(Projection proj) {
+			Expression e = proj.getOperand();
+			List<String> attributes = proj.getAttributes();
+			Formula f = translateToRC(e);
+			Set<Term> free = f.free();
+			for (Term term : free) {
+				String attr = env.get(term.toString());
+				if (attributes.contains(attr)) {
+					free.remove(term);
+				}
+			}
+			return new Existential(free, f);
+	}
+	
+	private Formula selectionToRC(Selection selec) { //  Not Done yet
+		Expression e = selec.getOperand();
+		Condition cond = selec.getCondition();
+		
+		Set<String> attrs; // attributes appearing in given condition
+		
+		Formula f = translateToRC(e);
+		Set<Term> terms;
+		for (String attr : attrs) {
+			terms.add(env2.get(attr)); // env2 maps attribute to variable (we need to create this)
+		}
+		Formula e1 = new Predicate(terms)
+		Formula e2 = new Equality(terms[0], terms[1]);
+		return new Conjunction(e1, e2);
+	}
+	
+	public static Formula translateToRC( Expression e ) {
+		if (e instanceof BaseExpression) {
+			return baseExprToRC((BaseExpression) e);
+		} else if (e instanceof Projection) {
+			return projectionToRC((Projection) e);
+		} else if (e instanceof Selection) {
+			return selectionToRC((Selection) e);
+		} else if (e instanceof Product) {  // not implemented yet
+			return productToRC((Product) e);
+		} else if (e instanceof Union) {  // not implemented yet
+			return unionToRC((Union) e);
+		} else if (e instanceof Difference) {  // not implemented yet
+			return differenceToRC((Difference) e);
+		} else { // we should never reach this point
+			throw new RuntimeException("Unknown kind of formula");
+		}
+	}
 }
