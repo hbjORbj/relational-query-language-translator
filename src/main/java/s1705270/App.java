d595ca1403c0ce1ec3e8ab3323bbc862d1c4a6ec
hbjORbj <sldisek783@gmail.com>
2021-03-04 00:31:05 +0000

    Completed RA->RC Translation Test

--- src/main/java/s1705270/App.java
+++ src/main/java/s1705270/App.java
@@ -1,6 +1,5 @@
 package s1705270;
 
-import java.io.File;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -14,58 +13,7 @@ import uk.ac.ed.pguaglia.real.lang.ReplacementException;
 public class App {
 
 	public static void main(String[] args) throws RecognitionException, ReplacementException {
-//		Term x = new Term("x", false);
-//		Term y = new Term("y", false);
-//		Term c = new Term("c", true);
-//
-//		List<Term> terms = new ArrayList<>();
-//		terms.add(x);
-//		terms.add(y);
-//		Predicate p = new Predicate("P", terms);
-//
-//		Equality eq1 = new Equality(x, c);
-//		Equality eq2 = new Equality(y, c);
-//		Disjunction disj = new Disjunction(eq1, eq2);
-//
-//		Term t = new Term("x", true);
-//		List<Term> terms2 = new ArrayList<>();
-//		terms2.add(t);
-//		Formula f = new Existential(terms2, new Conjunction(p, disj));
-//
-//		System.out.println(f);
-//		System.out.println(f.free());
-//		HashMap<String,List<String>> map = new HashMap<String, List<String>>();
-//		map.put("R", Arrays.asList(new String[] {"A","B"}));
-//		Schema sch = new Schema(map);
-//		System.out.println(sch.convert());
-//		Formula f1 = Formula.parse("?x = c)");
-//		System.out.println(f1);
-//		TranslatorRC trans = new TranslatorRC(sch);
-//		System.out.println(trans.Adom("N"));
-//		try {
-//			Expression exp  = trans.translate(f1);
-//			System.out.println(exp);
-//		} catch (TranslationException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		Formula f2 = Formula.parse("[E]?x,?y(R(?x,?y))");
-//		System.out.println(f2);
-		
-//		HashMap<String,List<String>> map = new HashMap<String, List<String>>();
-//		map.put("Customer", Arrays.asList(new String[] {"CustID","Name"}));
-//		map.put("Account", Arrays.asList(new String[] {"Number", "CustID"}));
-//		Schema sch = new Schema(map);
-//		TranslatorRC trans = new TranslatorRC(sch);
-//		Formula f1 = Formula.parse("[E]?x4(Customer(?x1,?x2) & Account(?x3,?x4) & ?x1 = ?x4)");
-//
-//		try {
-//			System.out.println(trans.translate(f1));
-//		} catch (TranslationException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-
+// 		RC -> RA Example
 //		HashMap<String,List<String>> map1 = new HashMap<String, List<String>>();
 //		map1.put("R", Arrays.asList(new String[] {"A", "B"}));
 //		Schema sch1 = new Schema(map1);
@@ -79,35 +27,19 @@ public class App {
 //			e.printStackTrace();
 //		}
 		
+		// RA -> RC Example
 		HashMap<String,List<String>> map = new HashMap<String, List<String>>();
-		map.put("R", Arrays.asList(new String[] {"A", "B"}));
-		map.put("S", Arrays.asList(new String[] {"B", "C"}));
+		map.put("R", Arrays.asList(new String[] {"A", "B","C"}));
+		map.put("S", Arrays.asList(new String[] {"C"}));
 		Schema sch = new Schema(map);
-		TranslatorRC trans = new TranslatorRC(sch);
-		//Formula f = Formula.parse("[E]?y( R(?y,?x) & ~( [E]?z (S(?x,?z) & ~(R(?z,?y)) )))");
-		Formula f = Formula.parse("[E]?C'( R(?A,?C,?C') )");
-		System.out.println(f);
-//		try {
-//			System.out.println(trans.translate(f));
-//		} catch (TranslationException e) {
-//			e.printStackTrace();
-//		}
-		
-		// Check the following test case with Professor
-//		HashMap<String,List<String>> map = new HashMap<String, List<String>>();
-//		map.put("R", Arrays.asList(new String[] {"A", "B","C"}));
-//		map.put("S", Arrays.asList(new String[] {"C"}));
-//		Schema sch = new Schema(map);
-//		TranslatorRA trans = new TranslatorRA(sch);
-////		Expression e = Expression.parse("<R>[A->B,B->C,C->A](R)");
-////		Expression e = Expression.parse("<R>[C->A](<R>[B->C](<R>[A->B](R)))");
-//		Expression e = Expression.parse("<R>[B->C](<P>[A,B](R))");
-//		try {
-//			e.signature(sch.convert());
-//			System.out.println(trans.translate(e));
-//		} catch (TranslationException | SchemaException error) {
-//			error.printStackTrace();
-//		}
+		TranslatorRA trans = new TranslatorRA(sch);
+		Expression e = Expression.parse("<R>[B->C](<P>[A,B](R)) <D> <S>[A = C | A = '1'](<P>[A](R) <X> (S))");
+		try {
+			e.signature(sch.convert());
+			System.out.println(trans.translate(e));
+		} catch (TranslationException | SchemaException error) {
+			error.printStackTrace();
+		}
 		
 	}
 }
--- src/test/java/s1705270/TranslationTest.java
+++ src/test/java/s1705270/TranslationTest.java
@@ -13,23 +13,9 @@ import java.util.Scanner;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import uk.ac.ed.pguaglia.real.db.SchemaException;
 import uk.ac.ed.pguaglia.real.lang.Expression;
 import uk.ac.ed.pguaglia.real.lang.ReplacementException;
-
-// 2017 December
-// RC -> RA
-// RC: "R(?x) | ([E]?z(S(?x,?z)) & ~(R(?z)))"
-// RA: "( <R>[B->Ax]( R ) <X> Adom_Az ) <U> ( ( <P>[Ax]( <R>[A->Ax,C->Az]( S ) ) <X> Adom_Az ) <I> ( ( Adom_Az <D> <R>[B->Az]( R ) ) <X> Adom_Ax ) )"
-
-// 2018 August
-// RC -> RA 
-// RC: "~([E]?y(~([E]?z(~(R(?x,?y)) | R(?y,?z)))))"
-// Output RA: "( Adom_Ax <X> Adom_Ay ) <D> <P>[Ax,Ay]( ( Adom_Ax <X> Adom_Ay ) <D> <P>[Ax,Ay]( ( ( ( Adom_Ax <X> Adom_Ay ) <D> <R>[A->Ax,B->Ay]( R ) ) <X> Adom_Az ) <U> ( <R>[A->Ay,B->Az]( R ) <X> Adom_Ax ) ) )"
-
-// 2018 August
-// RA -> RC
-// RA:
-// RC: "(R(?x,?y,?z) & ~(S(?z) & [E]?z(R(?x,?y,?z)))) & (S(?x) & [E]?x(R(?x,?y,?z)) & (?x = ?z | ?x = ?y))"
 
 public class TranslationTest {
 
@@ -37,33 +23,21 @@ public class TranslationTest {
 	static HashMap<Expression,Formula> translationPairsRA = new HashMap<>();
 	static HashMap<Formula, Schema> schemaPairsRC = new HashMap<>();
 	static HashMap<Expression, Schema> schemaPairsRA = new HashMap<>();
-
-	// take care of schemas
-	// maybe another file needed for RA/RC pairs
-//	public TranslationTest(File f) throws FileNotFoundException { 
-		// read file f with pairs (stringRC_i, stringRA_i) of RC/RA strings
-		// for each pair (stringRC_i, stringRA_i):
-		//   parse RC string stringRC_i to get Formula formula_i
-		//   parse RA string stringRA_i to get Expression expr_i
-		//   put (formula_i,expr_i) into translationPairs map
-		
-		// similarly for RA->RC
-//	}
 	
 	@BeforeAll
 	public static void prepareTestCases() throws FileNotFoundException, ReplacementException {
-		Scanner translations = new Scanner(new File(System.getProperty("user.dir") + "/src/main/resources/translations.txt"));
-		Scanner schemas = new Scanner(new File(System.getProperty("user.dir") + "/src/main/resources/schemas.txt"));
-		while (translations.hasNextLine()) {
-			String stringRC = translations.nextLine();
+		Scanner translationsRC = new Scanner(new File(System.getProperty("user.dir") + "/src/main/resources/translationsRC.txt"));
+		Scanner schemasRC = new Scanner(new File(System.getProperty("user.dir") + "/src/main/resources/schemasRC.txt"));
+		while (translationsRC.hasNextLine()) {
+			String stringRC = translationsRC.nextLine();
 			Formula f = Formula.parse(stringRC);
-			if (translations.hasNextLine()) {
-				String stringRA = translations.nextLine();
+			if (translationsRC.hasNextLine()) {
+				String stringRA = translationsRC.nextLine();
 				Expression e = Expression.parse(stringRA);
 				translationPairsRC.put(f, e);
 			}
-			if (schemas.hasNextLine()) {
-				String schema = schemas.nextLine();
+			if (schemasRC.hasNextLine()) {
+				String schema = schemasRC.nextLine();
 				String[] relations = schema.split(",");
 				HashMap<String,List<String>> map = new HashMap<String, List<String>>();
 
@@ -76,28 +50,58 @@ public class TranslationTest {
 				schemaPairsRC.put(f, new Schema(map));
 			}
         }
+		
+		Scanner translationsRA = new Scanner(new File(System.getProperty("user.dir") + "/src/main/resources/translationsRA.txt"));
+		Scanner schemasRA = new Scanner(new File(System.getProperty("user.dir") + "/src/main/resources/schemasRA.txt"));
+		while (translationsRA.hasNextLine()) {
+			String stringRA = translationsRA.nextLine();
+			Expression e = Expression.parse(stringRA);
+			if (translationsRA.hasNextLine()) {
+				String stringRC = translationsRA.nextLine();
+				Formula f = Formula.parse(stringRC);
+				translationPairsRA.put(e, f);
+			}
+			if (schemasRA.hasNextLine()) {
+				String schema = schemasRA.nextLine();
+				String[] relations = schema.split(",");
+				HashMap<String,List<String>> map = new HashMap<String, List<String>>();
+
+				for (String relation : relations) {
+					Integer idx = relation.indexOf(":");
+					String name = relation.substring(0, idx);
+					String attrString = relation.substring(idx + 1);
+					map.put(name, Arrays.asList(attrString.split(" ")));
+				}
+				schemaPairsRA.put(e, new Schema(map));
+			}
+        }
 	}
 	
 	@Test
 	public void RCTranslationTest() {
-		// for each pair (formula_i,expr_i):
-		//   translate formula_i to trans_expr_i
-		//   compare trans_expr_i.toString() with expr_i.toString()
 		for (Formula f : translationPairsRC.keySet()) {
 			try {
 				Schema sch = schemaPairsRC.get(f);
-				TranslatorRC trans = new TranslatorRC(sch);
-				assertEquals(trans.translate(f).toString(), translationPairsRC.get(f).toString());
-			} catch (TranslationException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				TranslatorRC transRC = new TranslatorRC(sch);
+				assertEquals(transRC.translate(f).toString(), translationPairsRC.get(f).toString());
+			} catch (TranslationException error) {
+				error.printStackTrace();
 			}
 		}
 	}
 	
 	@Test
 	public void RATranslationTest() {
-		// similar to RCTranslationTest
+		for (Expression e : translationPairsRA.keySet()) {
+			try {
+				Schema sch = schemaPairsRA.get(e);
+				e.signature(sch.convert());
+				TranslatorRA transRA = new TranslatorRA(sch);
+				assertEquals(transRA.translate(e).toString(), translationPairsRA.get(e).toString());
+			} catch (TranslationException | SchemaException error) {
+				error.printStackTrace();
+			}
+		}
 	}
 }
 
